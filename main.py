9eabe6655a0d20750b647251be3ffab2bc5d9078
Tom Draper <tomjdraper1@gmail.com>
2022-05-07 18:01:38 +0100

    working

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 from sklearn.cluster import KMeans
-import matplotlib.pyplot as plt
-import png
 
 
 def colour_clusters(X, n_clusters=5):
     kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(X)
     colours = kmeans.cluster_centers_
-    for colour in colours:
-        print(colour)
-    return colours
+    return colours.astype(int)
 
 def load_image(path):
     im = Image.open(path)
@@ -21,29 +17,43 @@ def build_colour_blocks(img_width, colours):
     block_size = img_width // len(colours)
     remaining = img_width - (block_size * len(colours))
     colour_blocks = np.full((block_size, block_size, 3), colours[0])
-    
     for i, colour in enumerate(colours[1:]):
         if i == len(colours)-2:
             block = np.full((block_size, block_size+remaining, 3), colour)
         else:
             block = np.full((block_size, block_size, 3), colour)
         colour_blocks = np.concatenate((colour_blocks, block), axis=1)
+        
     colour_blocks = np.array(colour_blocks, dtype=np.uint8)
     
     return colour_blocks
 
+def colour_code(rgb):
+    return ('#%02x%02x%02x' % rgb).upper()
+
+def draw_colour_code(draw, rgb, N, img_height, block_size):
+    font = ImageFont.truetype("Lato/Lato-Bold.ttf", size=block_size//10)
+    hex_colour = colour_code(rgb)
+    draw.text((block_size//2 - (block_size//4.7) + N * block_size, img_height + (block_size//2) - (block_size//20)), 
+              hex_colour, 
+              'black', 
+              font=font)
+
 def run():
+    print('Running...')
     arr = load_image('wave.jpg')
-    print(arr.shape)
+    print('Image size:', arr.shape)
     X = arr.reshape(-1, arr.shape[-1])  # Create single vector of all rgb values
 
     colours = colour_clusters(X)
+    print('Colours used:', [tuple(colour) for colour in colours])
     
     colour_blocks = build_colour_blocks(arr.shape[1], colours)
     img = Image.fromarray(np.concatenate((arr, colour_blocks), axis=0))
     
     draw = ImageDraw.Draw(img)
-    draw.text((100, 100), 'hidsgfsdagdsagfdsafdfasdf', 'black')
+    for i, rgb in enumerate(colours):
+        draw_colour_code(draw, tuple(rgb), i, arr.shape[0], arr.shape[1]//len(colours))
     
     img.save('colours.png')
 
